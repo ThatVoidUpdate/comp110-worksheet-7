8c44c35e72acefc331d5ca4cb961e0e2123d0371
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-11-25 15:44:38 +0000

    Added better comments

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -13,11 +13,11 @@ namespace comp110_worksheet_7
         {
             List<string> ret = new List<string>();
             foreach (string item in Directory.GetFileSystemEntries(filePath))
-            {
+            {//loop over every path in the directory
                 if (IsDirectory(item))
-                {
+                {//if it is a directory, then also search that
                     if (includeFolders)
-                    {
+                    {//if we want to return folders, add it to the return variable
                         ret.Add(item);
                     }
                     
@@ -26,7 +26,7 @@ namespace comp110_worksheet_7
                 else
                 {
                     if (includeFiles)
-                    {
+                    {//if we want to return files, add it to the return variable
                         ret.Add(item);
                     }
                 }
@@ -52,7 +52,7 @@ namespace comp110_worksheet_7
 		{
             long ret = 0;
             foreach (string file in Traverse(directory, true, false))
-            {
+            {//add the size of every file to the return variable
                 ret += GetFileSize(file);
             }
             return ret;
@@ -60,7 +60,7 @@ namespace comp110_worksheet_7
 
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
-		{
+		{//return the amount of entries when searching for only files
             return Traverse(directory, true, false).Count;
 		}
 
@@ -69,9 +69,10 @@ namespace comp110_worksheet_7
 		{
             int baseLength = directory.Split('\\').Length;
             int maxLength = 0;
+            //calculate a base length to be subtracted from everything else
 
             foreach (string file in Traverse(directory, true, false))
-            {
+            {//for every file, subtract the base amount of '\' from the current amount of '\', which gives the depth
                 if (file.Split('\\').Length - baseLength > maxLength)
                 {
                     maxLength = file.Split('\\').Length - baseLength;
@@ -87,14 +88,14 @@ namespace comp110_worksheet_7
             string SmallestPath = "";
             long SmallestSize = long.MaxValue;
             foreach (string path in Traverse(directory, true, false))
-            {
+            {//for every file, if it is smaller that the previous best, then store it
                 if (GetFileSize(path) < SmallestSize)
                 {
                     SmallestPath = path;
                     SmallestSize = GetFileSize(path);
                 }
             }
-
+            //return the smallest
             return new Tuple<string, long>(SmallestPath, SmallestSize);
 		}
 
@@ -104,14 +105,14 @@ namespace comp110_worksheet_7
             string LargestPath = "";
             long LargestSize = long.MinValue;
             foreach (string path in Traverse(directory, true, false))
-            {
+            {//for every file, if it is bigger that the previous best, then store it
                 if (GetFileSize(path) > LargestSize)
                 {
                     LargestPath = path;
                     LargestSize = GetFileSize(path);
                 }
             }
-
+            //return the largest
             return new Tuple<string, long>(LargestPath, LargestSize);
         }
 
@@ -121,13 +122,13 @@ namespace comp110_worksheet_7
             List<string> ret = new List<string>();
 
             foreach (string path in Traverse(directory, true, false))
-            {
+            {//for every file, if the size is correct, add it to be returned
                 if (GetFileSize(path) == size)
                 {
                     ret.Add(path);
                 }
             }
-
+            //return them
             return ret;
         }
 	}
